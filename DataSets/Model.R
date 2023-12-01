a62593f2fea98c2a951b8db764e8e0a7c9ac273f
Jennifer Broatch <jenn9@asurite.asu.edu>
2023-12-01 10:46:52 -0700

    copy file

--- DataSets/Model.R
+++ DataSets/Model.R
@@ -1,5 +1,110 @@
 # ABOUT ---
+# LOAD LIBRARIES ---
+library(tidyverse)
+library(lubridate)
+library(dplyr)
+library(zoo)
+library(lme4)
+library(glme)
+#ibrary(DescTools)
+library(nlme)
 
+# LOAD DATA ---
+bfly<- read.csv(file = "DataSets/TotalButterflyWithFamily.csv")
+View(bfly)
+
+Total_butterfly <- bfly %>% 
+  select(Year, Month, Day, Site, ButterflyCount) %>% 
+  group_by(Year, Month, Day, Site) %>% 
+  summarize(total_butterly_count = sum(ButterflyCount)) 
+View(Total_butterfly)
+
+# Creating a data file with Total unique butterfly species for each outing 
+Total_butterfly2 <- bfly %>% 
+  select(Year, Month, Day, Site, NABAEnglishName, Family) %>% 
+  group_by(Year, Month, Day, Site) %>% 
+  summarize(Unique_butterflies = n_distinct(NABAEnglishName))
+
+
+View(Total_butterfly2)
+# SEPARATE DATA INTO FALL AND SPRING DATA ---
+
+# Creating the fall DF
+bfly_fall <- bfly_analysis %>% 
+  select(Year:Family) %>% 
+  filter(Month > 6)
+View(bfly_fall)
+
+# Removing second sampling of santarita mountains
+bfly_fall <- bfly_fall[!(bfly_fall$Site == 'SantaRitaMountains' & bfly_fall$Month == 9),]
+bfly_fall <- bfly_fall[!(bfly_fall$Site == 'SantaRitaMountains' & bfly_fall$Month == 10),]
+
+# Removing pre 7/15 sampling
+bfly_fall <- bfly_fall[!(bfly_fall$Site == 'GrandCanyonNorthRim' & bfly_fall$Day == 5),]
+bfly_fall <- bfly_fall[!(bfly_fall$Site == 'SycamoreCreekAZ' & bfly_fall$Day == 7),]
+
+# Creating the spring data observations
+bfly_spring <- bfly_analysis %>% 
+  select(year:TotalDistanceMi) %>% 
+  filter(month < 7)
+
+# Removing post 6/15 sampling  
+bfly_spring <- bfly_spring[!(bfly_spring$Site == 'RamseyCanyonAZ' & bfly_spring$month == 6),]
+bfly_spring <- bfly_spring[!(bfly_spring$Site == 'AtascosaHighlandsAZ' & bfly_spring$month == 6),]
+
+# Save files
+write_csv(x = bfly_spring, 
+          file = "data/butterfly_analysis_spring.csv")
+
+write_csv(x = bfly_fall, 
+          file = "data/butterfly_analysis_fall.csv")
+
+
+
+# GENERATE MODELS ---
+
+#Attempting to set up a basic model 
+model = lmer(total_butterfly_count ~ year + (1|Site),
+             data=bfly_spring,
+             REML = TRUE)
+summary(model)
+
+model2 = lmer(total_butterfly_count ~ Year + (1|Site),
+              data=bfly_fall,
+              REML = TRUE)
+summary(model2)
+
+
+
+model3 = lme(fixed=total_butterfly_count~year, data=bfly_fall, random= ~1|Site)
+summary(model3)
+
+model3
+
+model4 = lme(fixed=total_butterfly_count~year+ tmean_previous30 + tmin_previous30 + tmax_previous30, data=bfly_fall, random= ~1|Site)
+summary(model4)
+plot(model4)
+
+model5 = lme(fixed=log(total_butterfly_count)~year+ tmean_previous30 + tmin_previous30 + tmax_previous30, data=bfly_fall, random= ~1|Site)
+plot(model5)
+summary(model5)
+
+model6 = lme(fixed=log(total_butterfly_count)~year+ tmean_previous30 + tmin_previous30 + tmax_previous30 + PrecipSum_previous90, data=bfly_fall, random= ~1|Site) 
+summary(model6)
+plot(model6)
+
+model7 = lme(log(total_butterfly_count)~year+ tmean_previous30 + tmin_previous30 + tmax_previous30 + PrecipSum_previous90 + (1|Site) + (1|year), data=bfly_fall) 
+
+model8 = lme(fixed=log(total_butterfly_count)~year+ tmean_previous30 + tmin_previous30 + tmax_previous30 + PrecipSum_previous90, data=bfly_fall, random=list(~1|year, ~1|Site)) 
+summary(model8)
+plot(model8)
+
+model9 = update(model8, correlation= corAR1())
+
+plot(model9)
+model9$varFix
+
+cov2cor(vcov(model9))
 
 # LOAD LIBRARIES ---
 library(tidyverse)
